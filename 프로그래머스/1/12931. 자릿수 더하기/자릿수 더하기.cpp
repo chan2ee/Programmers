adb770e5c6e38137ab3b4cc4ea67f859f9537114
chan2ee <99484214+chan2ee@users.noreply.github.com>
2024-03-18 22:34:48 +0900

    Update 자릿수 더하기.cpp

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -6,8 +6,8 @@ int solution(int n)
     int answer = 0;
     
     while(n > 0){
-        answer += n % 10;
-        n /= 10;
+        answer += n % 10;    //어떤 수를 10으로 나눈 나머지는 1의 자리 수가 된다.
+        n /= 10;            //어떤 수를 10으로 나눈 몫은 1의 자리를 제외한 수가 된다.
     }
     
 
