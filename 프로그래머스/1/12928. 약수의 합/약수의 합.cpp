5bf4b6b296feb1db25399b880522dd1a2d30a2aa
chan2ee <99484214+chan2ee@users.noreply.github.com>
2024-03-18 22:34:02 +0900

    Update 약수의 합.cpp

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++){    //for문 범위를 1부터 하는 이유 : 0으로 나누면 예외처리가 되기 때문.
         if(n % i == 0){
             answer += i;
         }
@@ -14,3 +14,4 @@ int solution(int n) {
     
     return answer;
 }
+
