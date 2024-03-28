a2d85e2da563a0588298b5c9497246b869420302
chan2ee <99484214+chan2ee@users.noreply.github.com>
2024-03-28 15:37:56 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    int sum = brown + yellow;
+    vector<int> v;
+    int a, b;
+    
+    for(int i = 1; i <= sum / i; i++){
+        if(sum % i == 0){
+            a = max(i, sum / i);
+            b = min(i, sum / i);
+            
+            if((a-2) * (b-2) == yellow){
+                answer.push_back(a);
+                answer.push_back(b);
+            }
+        }    
+    }
+    return answer;
+}
